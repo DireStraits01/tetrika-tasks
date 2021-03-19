14e4811718e30e1d8752592f4170483898d90ada
DireStraits01 <arsamak88@gmail.com>
2021-03-19 09:40:27 +0300

    solution for 3 task

--- task3.py
+++ task3.py
@@ -1,46 +1,43 @@
+import time
+
 
 def appearance(intervals):
-    lesson_times = dc['lesson']
-    pupil_times = dc['pupil']
-    tutor_times = dc['tutor']
-    total_time_pupil = 0
-    start_time_pupil = 0
-    end_time_pupil = 0
+    puptimes = dc['pupil'][:]
+    tuttimes = dc['tutor'][:]
+    pupil_in = False
+    tutor_in = False
 
-    if len(pupil_times) > len(tutor_times):
-        for index, times in pupil_times
-
-    for index, time in enumerate(pupil_times):
-        if (index % 2) == 0:
-           start_time_pupil = time
+    last = 0
+    together = 0
+    while puptimes and tuttimes:
+        # Pick the event to come next.
+        if puptimes[0] < tuttimes[0]:
+            evt = puptimes.pop(0)
+            pupil_in = not pupil_in
         else:
-            end_time_pupil = time
+            evt = tuttimes.pop(0)
+            tutor_in = not tutor_in
 
-        if start_time_pupil != 0 and end_time_pupil !=0:
-            total_time_pupil += (end_time_pupil - start_time_pupil)
-            start_time_pupil = 0
-            end_time_pupil = 0
-
-    tutor_times = dc['tutor']
-    total_time_tutor = 0
-    start_time_tutor = 0
-    end_time_tutor = 0
-    for index, time in enumerate(tutor_times):
-        if (index % 2) == 0:
-            start_time_tutor = time
+        tc = time.ctime(evt)
+        if pupil_in and tutor_in:
+            print( tc, "Both are in the room." )
+            last = evt
         else:
-            end_time_tutor = time
+            if last:
+                print( tc, "No longer both in, together time =", evt-last )
+                together += evt-last
 
-        if start_time_tutor != 0 and end_time_tutor != 0:
-            total_time_tutor += (end_time_tutor - start_time_tutor)
-            start_time_tutor = 0
-            end_time_tutor = 0
-    return total_time_tutor + total_time_pupil
+            if pupil_in:
+                print( tc, "Pupil is in the room alone" )
+            elif tutor_in:
+                print( tc, "Tutor is in the room alone" )
+            else:
+                print( tc, "Room is empty" )
 
+    return together
 
 dc = {
-  'lesson': [1594663200, 1594666800],
-  'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
-  'tutor': [1594663290, 1594663430, 1594663443, 1594666473]}
+      'lesson': [1594663200, 1594666800],
+      'pupil': [1594663340, 1594663389, 1594663390, 1594663395, 1594663396, 1594666472],
+      'tutor': [1594663290, 1594663430, 1594663443, 1594666473]}
 print(appearance(dc))
-
